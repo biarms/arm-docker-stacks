24940708927bd5e582bb9ec93ebe9d66cfd251d4
olivier <olivier.kremer@free.fr>
2020-05-20 14:40:20 +0200

    Remove k8s files

--- wordpress/k8s/startWordpress.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-kubectl scale deployment wordpress --replicas=2
-kubectl scale deployment wordpress-db --replicas=1
--- wordpress/k8s/stopWordpress.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-kubectl scale -n default deployment wordpress --replicas=0
-kubectl scale -n default deployment wordpress-db --replicas=0
